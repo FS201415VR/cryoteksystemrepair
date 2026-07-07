4f82f168aef51e35c9edb38f74adf22918d9a1bf
FSVR <bubbajohn937@gmail.com>
2026-07-07 12:10:33 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log("Cryotek System Repair Loaded!");
+
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("active");
+    document.querySelector(".hamburger").classList.toggle("active");
+}
+
+function toggleTheme() {
+    document.body.classList.toggle("light-mode");
+}
